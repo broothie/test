7a4b3391c2ea66a560cd48b9c7f88ff3c1f11660
Andrew Booth <adbooth8@gmail.com>
2025-02-26 19:31:49 -0800

    fix equal comparison (#5)

--- equal.go
+++ equal.go
@@ -12,21 +12,31 @@ import (
 func Equal[T comparable](t TestingT, actual, expected T) bool {
 	t.Helper()
 
-	return assert(t, actual == expected, fmt.Sprintf("%v to == %v:\n%s", actual, expected, diff.Diff(spew.Sdump(actual), spew.Sdump(expected))))
+	var zero T
+	if _, ok := any(zero).(string); ok {
+		return assert(t, actual == expected, fmt.Sprintf("actual to equal expected:\n%s", diff.Diff(any(actual).(string), any(expected).(string))))
+	}
+
+	return assert(t, actual == expected, fmt.Sprintf("actual to equal expected:\n%s", diff.Diff(spew.Sdump(actual), spew.Sdump(expected))))
 }
 
 // NotEqual asserts that two comparable values are not equal.
 func NotEqual[T comparable](t TestingT, actual, expected T) bool {
 	t.Helper()
 
-	return assert(t, actual != expected, fmt.Sprintf("%v to == %v", actual, expected))
+	return assert(t, actual != expected, fmt.Sprintf("%v to not equal %v", actual, expected))
 }
 
 // DeepEqual asserts that two values are deeply equal using reflect.DeepEqual.
-func DeepEqual(t TestingT, actual, expected any) bool {
+func DeepEqual[T any](t TestingT, actual, expected T) bool {
 	t.Helper()
 
-	return assert(t, reflect.DeepEqual(actual, expected), fmt.Sprintf("%v to deep equal %v:\n%s", actual, expected, diff.Diff(spew.Sdump(actual), spew.Sdump(expected))))
+	var zero T
+	if _, ok := any(zero).(string); ok {
+		return assert(t, reflect.DeepEqual(actual, expected), fmt.Sprintf("actual to deep equal expected:\n%s", diff.Diff(any(actual).(string), any(expected).(string))))
+	}
+
+	return assert(t, reflect.DeepEqual(actual, expected), fmt.Sprintf("actual to deep equal expected:\n%s", diff.Diff(spew.Sdump(actual), spew.Sdump(expected))))
 }
 
 // NotDeepEqual asserts that two values are not deeply equal.
